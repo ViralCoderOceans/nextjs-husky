43e1247e7b230eac0b15670185c208442c985765
Viral Nakrani <viral.coderoceans@gmail.com>
2023-08-26 12:18:50 +0530

    test case 6

--- src/app/a/page.js
+++ src/app/a/page.js
@@ -3,14 +3,13 @@
 import React, { useState } from "react";
 
 const Page = () => {
-    const [name] = useState("A");
-    return (
-        <div>
-            <h1 className="text-center text-3xl font-bold">
-                This is page {name}
-            </h1>
-        </div>
-    );
+  // const [name] = useState("A");
+  const [name] = useState("A");
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-bold">This is page {name}</h1>
+    </div>
+  );
 };
 
 export default Page;

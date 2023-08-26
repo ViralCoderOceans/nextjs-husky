840ea62f6e230ef9eac9887a1fbfc8b26a05ecc3
Viral <viral.coderoceans@gmail.com>
2023-08-26 11:55:35 +0530

    test 2

--- src/app/a/page.js
+++ src/app/a/page.js
@@ -1,14 +1,16 @@
-"use client"
+'use client';
 
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const Page = () => {
-  const [name] = useState('A')
-  return (
-    <div>
-      <h1 className='text-center text-3xl font-bold'>This is page {name}</h1>
-    </div>
-  )
-}
+    const [name] = useState('A');
+    return (
+        <div>
+            <h1 className='text-center text-3xl font-bold'>
+                This is page {name}
+            </h1>
+        </div>
+    );
+};
 
-export default Page
+export default Page;
--- src/app/b/page.js
+++ src/app/b/page.js
@@ -1,15 +1,17 @@
-"use client"
+'use client';
 
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const Page = () => {
-  const [name] = useState('B')
-  return (
-    <div>
-      <h1 className='text-center text-3xl font-bold'>Test Page</h1>
-      <h1 className='text-center text-3xl font-bold'>This is page {name}</h1>
-    </div>
-  )
-}
+    const [name] = useState('B');
+    return (
+        <div>
+            <h1 className='text-center text-3xl font-bold'>Test Page</h1>
+            <h1 className='text-center text-3xl font-bold'>
+                This is page {name}
+            </h1>
+        </div>
+    );
+};
 
-export default Page
+export default Page;

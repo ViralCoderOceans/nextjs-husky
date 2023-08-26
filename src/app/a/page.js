3bd2fbbd9350cd8d61763915612b27e4fc4fe2ec
Viral <viral.coderoceans@gmail.com>
2023-08-26 11:34:45 +0530

    initial commit

---
+++ src/app/a/page.js
@@ -0,0 +1,14 @@
+"use client"
+
+import React, { useState } from 'react'
+
+const Page = () => {
+  const [name] = useState('A')
+  return (
+    <div>
+      <h1 className='text-center text-3xl font-bold'>This is page {name}</h1>
+    </div>
+  )
+}
+
+export default Page

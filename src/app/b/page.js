de8dc675b0cafd11cf6fe76eb3f3c3ddb2567e5f
Viral Nakrani <viral.coderoceans@gmail.com>
2023-08-26 12:10:20 +0530

    test case 3

--- src/app/b/page.js
+++ src/app/b/page.js
@@ -1,9 +1,9 @@
 "use client";
 
-import React, { useState } from "react"
+import React, { useState } from "react";
 
 const Page = () => {
-    const [name] = useState("B")
+    const [name] = useState("B");
     return (
         <div>
             <h1 className="text-center text-3xl font-bold">Test Page</h1>
